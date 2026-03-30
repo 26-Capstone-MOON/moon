2da13b31a085fa97999d04d805a95a7d8f850c60
ikoniccc <wegonegetitpopit@gmail.com>
2026-03-30 20:15:14 +0900

    벡앤드 수정

---
+++ backend/src/main/java/com/moonapp/common/config/WebSocketConfig.java
@@ -0,0 +1,14 @@
+package com.moonapp.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // Tracking handler is registered in Phase B4.
+    }
+}
---
+++ backend/src/main/java/com/moonapp/model/vo/Location.java
@@ -0,0 +1,23 @@
+package com.moonapp.model.vo;
+
+public class Location {
+
+    private Double latitude;
+    private Double longitude;
+
+    public Location() {
+    }
+
+    public Location(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+}
